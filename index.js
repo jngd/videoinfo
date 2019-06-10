a3a395cf2cb17d7edba5626b1cb07eb08f146b74
jngd <juangrdiaz@gmail.com>
2019-06-10 17:39:08 +0200

    First commit IPC finished

---
+++ index.js
@@ -0,0 +1,23 @@
+const electron = require('electron');
+const ffmpeg = require('fluent-ffmpeg');
+
+const { app, BrowserWindow, ipcMain } = electron;
+
+app.on('ready', () => {
+  const mainWindow = new BrowserWindow({
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+  mainWindow.loadURL(`file://${__dirname}/index.html`);
+});
+
+ipcMain.on('video:submit', (event, path) => {
+  ffmpeg.ffprobe(path, (err, metadata) => {
+    if (err) {
+      return;
+    }
+
+    console.log('File duration', metadata.format.duration);
+  });
+});
